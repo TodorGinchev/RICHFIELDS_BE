5385f12237220a8af196d52827fb41818018d902
Ginchev Todor <ginchet1@pc124.netlab.hut.fi>
2017-03-21 18:23:23 +0200

    auth almost done, small bug needs fixing

--- api/authapi/todo/web/router.ex
+++ api/authapi/todo/web/router.ex
@@ -7,18 +7,32 @@ defmodule Todo.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug Guardian.Plug.VerifySession
+    plug Guardian.Plug.LoadResource
+  end
+
+  pipeline :browser_auth do
+    plug Guardian.Plug.VerifySession
+    plug Guardian.Plug.EnsureAuthenticated, handler: Todo.Token
+    plug Guardian.Plug.LoadResource
   end
 
   pipeline :api do
     plug :accepts, ["json"]
   end
 
-  scope "/", Todo do
-    pipe_through :browser # Use the default browser stack
+ scope "/", Todo do
+  pipe_through :browser # Use the default browser stack
+  get "/", PageController, :index
+  resources "/users", UserController, only: [:new, :create]
+  resources "/sessions", SessionController, only: [:new, :create,  
+                                                   :delete]
+end
+scope "/", Todo do
+  pipe_through [:browser, :browser_auth]
+  resources "/users", UserController, only: [:show, :index, :update]
+end
 
-    get "/", PageController, :index
-    resources "/users", UserController
-  end
 
   # Other scopes may use custom stacks.
   # scope "/api", Todo do
--- api/authapi/todo/web/web.ex
+++ api/authapi/todo/web/web.ex
@@ -78,4 +78,26 @@ defmodule Todo.Web do
   defmacro __using__(which) when is_atom(which) do
     apply(__MODULE__, which, [])
   end
+
+
+
+
+
+def view do
+  quote do
+    use Phoenix.View, root: "web/templates"
+    # Import convenience functions from controllers
+    import Phoenix.Controller, only: [get_csrf_token: 0, 
+                               get_flash: 2, view_module: 1]
+    # Use all HTML functionality (forms, tags, etc)
+    use Phoenix.HTML
+    import Todo.Router.Helpers
+    import Todo.ErrorHelpers
+    import Todo.Gettext
+    import Todo.ViewHelper
+  end
+end
+
+
+
 end
